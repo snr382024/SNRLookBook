4649a84df6f192b00750db034e9aa22d8922dfbd
Silver Needle 38 <166210222+snr382024@users.noreply.github.com>
2024-05-01 22:10:45 -0400

    Update CoverPage.tsx

--- SNR38Site/src/pages/CoverPage/CoverPage.tsx
+++ SNR38Site/src/pages/CoverPage/CoverPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import CoverVideo from "../../assets/videos/CoverVideo.mp4"
 import "./coverpage.scss";
 
 const CoverPage: React.FC = () => {
   return (
     <div className="cover-page">
       <video autoPlay loop playsInline muted className="background-video">
-        <source src={CoverVideo} type="video/mp4" />
+        <source type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="video-overlay"></div>
