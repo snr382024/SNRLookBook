4d185c906771d06fce9d6757496c0814274df77f
connorjohnson6 <166210222+snr382024@users.noreply.github.com>
2024-05-03 10:36:16 -0400

    small change

--- SNR38Site/src/App.tsx
+++ SNR38Site/src/App.tsx
@@ -1,55 +1,57 @@
-import { Suspense, lazy } from 'react';
-import "./App.scss";
+import CoverPage from "./pages/CoverPage/CoverPage";
+import NameDisplay from "./pages/NameDisplay/NameDisplay";
 
-// Lazy load all pages
-const CoverPage = lazy(() => import('./pages/CoverPage/CoverPage'));
-const NameDisplay = lazy(() => import('./pages/NameDisplay/NameDisplay'));
-const Olivia = lazy(() => import('./pages/3OliviaCavallero/Olivia'));
-const Shirell = lazy(() => import('./pages/2ShirellBattle/Shirell'));
-const Aliah = lazy(() => import('./pages/4AliahChammoro/Aliah'));
-const Gavin = lazy(() => import('./pages/5GavinCooper/Gavin'));
-const Sera = lazy(() => import('./pages/6SeraCristinFerrante/Sera'));
-const Caitlin = lazy(() => import('./pages/7CaitlinFracasso/Caitlin'));
-const Annika = lazy(() => import('./pages/8AnnikaGeissberger/Annika'));
-const Alyssa = lazy(() => import('./pages/9AlyssaJones/Alyssa'));
-const Emma = lazy(() => import('./pages/10EmmaKeating/Emma'));
-const Shannon = lazy(() => import('./pages/11ShannonKonecke/Shannon'));
-const Madisyn = lazy(() => import('./pages/12MadisynMartinelli/Madisyn'));
-const Deanna = lazy(() => import('./pages/13DeannaMcnally/Deanna'));
-const Francesca = lazy(() => import('./pages/14FrancescaMorabito/Francesca'));
-const Eva = lazy(() => import('./pages/15EvaReed/Eva'));
-const David = lazy(() => import('./pages/16DavidRoot/David'));
-const Natalie = lazy(() => import('./pages/17NatalieSweeney/Natalie'));
-const Nicole = lazy(() => import('./pages/18NicoleTasca/Nicole'));
-const Mo = lazy(() => import('./pages/19MoWalsh/Mo'));
-const SierraWeissner = lazy(() => import('./pages/20SierraWeissner/Sierra'));
+import Olivia from "./pages/3OliviaCavallero/Olivia"; //Temp 1
+import Shirell from "./pages/2ShirellBattle/Shirell"; //Temp 5
+import Aliah from "./pages/4AliahChammoro/Aliah"; //Temp 3
+import Gavin from "./pages/5GavinCooper/Gavin"; //Temp 5
+import Sera from "./pages/6SeraCristinFerrante/Sera"; //Temp 4
+import Caitlin from "./pages/7CaitlinFracasso/Caitlin"; //Temp 2
+import Annika from "./pages/8AnnikaGeissberger/Annika"; //Temp 1
+import Alyssa from "./pages/9AlyssaJones/Alyssa"; //Temp 3
+import Emma from "./pages/10EmmaKeating/Emma"; //Temp 4
+import Shannon from "./pages/11ShannonKonecke/Shannon"; //Temp 1
+import Madisyn from "./pages/12MadisynMartinelli/Madisyn"; //Temp 3
+import Deanna from "./pages/13DeannaMcnally/Deanna"; //Temp 4
+import Francesca from "./pages/14FrancescaMorabito/Francesca"; //Temp 1
+import Eva from "./pages/15EvaReed/Eva"; //Temp 3
+import David from "./pages/16DavidRoot/David"; //Temp 4
+import Natalie from "./pages/17NatalieSweeney/Natalie"; //Temp 5
+import Nicole from "./pages/18NicoleTasca/Nicole"; //Temp 1
+import Mo from "./pages/19MoWalsh/Mo"; //Temp 3
+import SierraWeissner from "./pages/20SierraWeissner/Sierra"; //Temp 4
+
+
+
+import "./App.scss";
 
 function App() {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <CoverPage />
-        <NameDisplay />
-        <Olivia />
-        <Shirell />
-        <Aliah />
-        <Gavin />
-        <Sera />
-        <Caitlin />
-        <Annika />
-        <Alyssa />
-        <Emma />
-        <Shannon />
-        <Madisyn />
-        <Deanna />
-        <Francesca />
-        <Eva />
-        <David />
-        <Natalie />
-        <Nicole />
-        <Mo />
-        <SierraWeissner />
-      </Suspense>
+      <CoverPage />
+      <NameDisplay />
+
+      <Olivia />
+      <Shirell /> 
+      <Aliah />
+      <Gavin />
+      <Sera />
+      <Caitlin />
+      <Annika />
+      <Alyssa />
+      <Emma />
+      <Shannon />
+      <Madisyn />
+      <Deanna />
+      <Francesca />
+      <Eva />
+      <David />
+      <Natalie />
+      <Nicole />
+      <Mo />
+      <SierraWeissner />
+
+
     </div>
   );
 }
