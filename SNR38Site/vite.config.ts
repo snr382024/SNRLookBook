942252edf84e59312c448471951d8e5b37e0dc3a
connorjohnson6 <166210222+snr382024@users.noreply.github.com>
2024-05-04 11:29:20 -0400

    config changes

--- SNR38Site/vite.config.ts
+++ SNR38Site/vite.config.ts
@@ -9,7 +9,27 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: './index.html',
+        shirell: './shirell.html',
+        olivia: './olivia.html',
+        aliah: './aliah.html',
+        gavin: './gavin.html',
+        sera: './sera.html',
+        caitlin: './caitlin.html',
+        annika: './annika.html',
+        alyssa: './alyssa.html',
+        emma: './emma.html',
+        shannon: './shannon.html',
+        madisyn: './madisyn.html',
+        deanna: './deanna.html',
+        francesca: './francesca.html',
+        eva: './eva.html',
+        david: './david.html',
+        natalie: './natalie.html',
+        nicole: './nicole.html',
+        mo: './mo.html',
+        sierra: './sierra.html',
+
       }
     }
   },
